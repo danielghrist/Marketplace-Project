6b1764819805cce88b4563696a959fba822426e0
Ubuntu <ubuntu@ip-172-31-34-92.us-east-2.compute.internal>
2024-03-15 03:46:51 +0000

    Initial Commit

---
+++ assets/js/event-attributes.js
@@ -0,0 +1,50 @@
+/*** Using JavaScript and Bootstrap Input Validation States to modify input classes with errors: ***/
+
+// Test for validity of username length:
+function checkUsername() {
+  // Declare/Initialize variables:
+  var usernameDiv = document.getElementById("username-div");
+  var username = document.getElementById("username");
+  var unFeedbackSpan = document.getElementById("un-feedback");
+  var unGlyphiconSpan = document.getElementById("un-icon");
+
+  if (username.value.length < 5) {
+    usernameDiv.classList.add("has-error");
+    // usernameDiv.classList.add("has-feedback");
+    unGlyphiconSpan.classList.add("glyphicon-remove");
+
+    unFeedbackSpan.innerHTML =
+      "Your username must be at least five characters long.";
+  } else {
+    usernameDiv.classList.add("has-success");
+    unGlyphiconSpan.classList.add("glyphicon-ok");
+    unFeedbackSpan.innerHTML = "";
+
+    usernameDiv.classList.remove("has-error");
+    unGlyphiconSpan.classList.remove("glyphicon-remove");
+  }
+}
+
+// Test for validity of password length:
+function checkPassword() {
+  // Declare/Initialize variables:
+  var passwordDiv = document.getElementById("password-div");
+  var password = document.getElementById("password");
+  var pwGlyphiconSpan = document.getElementById("pw-icon");
+  var pwFeedbackSpan = document.getElementById("pw-feedback");
+
+  if (password.value.length < 5) {
+    passwordDiv.classList.add("has-error");
+    pwGlyphiconSpan.classList.add("glyphicon-remove");
+
+    pwFeedbackSpan.innerHTML =
+      "Your password must be at least five characters long.";
+  } else {
+    passwordDiv.classList.add("has-success");
+    pwGlyphiconSpan.classList.add("glyphicon-ok");
+    pwFeedbackSpan.innerHTML = "";
+
+    passwordDiv.classList.remove("has-error");
+    pwGlyphiconSpan.classList.remove("glyphicon-remove");
+  }
+}
