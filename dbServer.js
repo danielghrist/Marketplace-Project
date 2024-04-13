7919cc6c5a4d20632c8dfa55bf458f0e432b894b
Danny Ghrist <danielghrist@gmail.com>
2024-04-13 18:33:32 -0500

    Testing if createUser route is even being reached.

--- dbServer.js
+++ dbServer.js
@@ -40,39 +40,39 @@ const bcrypt = require("bcrypt");
 app.use(express.urlencoded(/*{ extended: true }*/));
 //middleware to read req.body.<params>
 //CREATE USER
-app.post("/register", async (req, res) => {
-  const user = req.body.username;
-  const hashedPassword = await bcrypt.hash(req.body.password, 10);
-  db.getConnection(async (err, connection) => {
-    if (err) throw err;
-    // ? will be replaced by values
-    // ?? will be replaced by string
-    const sqlSearch = "SELECT * FROM userTable WHERE user = ?";
-    const search_query = mysql.format(sqlSearch, [user]);
-    const sqlInsert = "INSERT INTO userTable VALUES (0,?,?)";
-    const insert_query = mysql.format(sqlInsert, [user, hashedPassword]);
+app.post("/createUser", async (req, res) => {
+  res.send("hello I am accesible!!!");
+  // const user = req.body.username;
+  // const hashedPassword = await bcrypt.hash(req.body.password, 10);
+  // db.getConnection(async (err, connection) => {
+  //   if (err) throw err;
+  //   // ? will be replaced by values
+  //   // ?? will be replaced by string
+  //   const sqlSearch = "SELECT * FROM userTable WHERE user = ?";
+  //   const search_query = mysql.format(sqlSearch, [user]);
+  //   const sqlInsert = "INSERT INTO userTable VALUES (0,?,?)";
+  //   const insert_query = mysql.format(sqlInsert, [user, hashedPassword]);
 
-    await connection.query(search_query, async (err, result) => {
-      if (err) throw err;
-      console.log("------> Search Results");
-      console.log(result.length);
-      if (result.length != 0) {
-        connection.release();
-        console.log("------> User already exists");
-        res.sendStatus(409);
-      } else {
-        await connection.query(insert_query, (err, result) => {
-          connection.release();
-          if (err) throw err;
-          console.log("--------> Created new User");
-          console.log(result.insertId);
-          res.sendStatus(200).send("User added to database...");
-        });
-      }
-    }); //end of connection.query()
-  }); //end of db.getConnection()
+  //   await connection.query(search_query, async (err, result) => {
+  //     if (err) throw err;
+  //     console.log("------> Search Results");
+  //     console.log(result.length);
+  //     if (result.length != 0) {
+  //       connection.release();
+  //       console.log("------> User already exists");
+  //       res.sendStatus(409);
+  //     } else {
+  //       await connection.query(insert_query, (err, result) => {
+  //         connection.release();
+  //         if (err) throw err;
+  //         console.log("--------> Created new User");
+  //         console.log(result.insertId);
+  //         res.sendStatus(201).send("User added to database...");
+  //       });
+  //     }
+  //   }); //end of connection.query()
+  // }); //end of db.getConnection()
 }); //end of app.post()
-
 /***** END CODE TO BE ABLE TO ADD ROUTE FOR REGISTRATION *****/
 
 const port = process.env.PORT;
